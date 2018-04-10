03b2aea1d4f3a7426d6c528013f95ce77d7ba816
Danni <dannielleorlando@gmail.com>
2018-04-10 16:54:04 -0400

    gets site addresses at all pages

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const cheerio = require('cheerio');
 const Promise = require('bluebird');
 
 const base = 'https://www.builtinnyc.com';
-const config = {
-  url: base + '/companies?status=all',
-};
 
 const companyMap = new Map();
 
-const getAllCompany = () => axios(config)
+// gets initial data for one page
+const loadPage = (page) => axios.get(base + page)
   .then(res => cheerio.load(res.data));
 
 // returns an array of companies with name and link tuples
@@ -28,24 +26,36 @@ const getSite = (name, link) => axios.get(base + link)
   .then(res => {
     let $ = cheerio.load(res.data);
     let website = $('a', '.field_company_website').attr('href');
+    console.log(website)
     companyMap.set(name, website);
   })
 
 const getSitePromises = (nameLink) =>
   nameLink.map((pair) => getSite(pair[0], pair[1]))
 
-// gets company info on one page, returns a cheerio object
-// getAllCompany()
-//   .then($ => Promise.all(getSitePromises((getNameLink($)))))
-//   .then(res => console.log(companyMap))
-//   .catch(err => console.log(err))
+// finds all available page links
+const loadPages = () =>
+  loadPage('/companies?status=all')
+    .then($ => {
+      let pages = []
+      $('a', '.pager__items').each((i, el) => {
+        let page = $(el).attr('href')
+        pages.push(page)
+      })
+      // last page is redundant
+      pages.pop()
+      console.log(pages)
+      return pages
+    })
 
-getAllCompany().then($ => {
-  let pages = []
-  $('a', '.pager__items').each((i, el) => {
-    let page = $(el).attr('href')
-    console.log(page)
-    pages.push(page)
-  })
 
-})
+// crawls a page to get all site addresses
+const getPageData = (page) =>
+  loadPage('/companies' + page)
+  .then($ => getSitePromises((getNameLink($))))
+
+loadPages()
+  .then(pages => Promise.all(pages.map((page) => getPageData(page))))
+  .then(console.log(companyMap))
+  .catch(err => console.log(err))
+
